d59f48a62f44a87a10ea15db50d0e353313d6f43
Onesinus SPT <onesinus@delman.io>
2023-10-16 21:29:34 +0700

    Practice CRUD using javascript and css framework

--- latihan-final-assesment/js/crud.js
+++ latihan-final-assesment/js/crud.js
@@ -6,12 +6,11 @@ ls_data[2] = {name: 'Bambang', age: 17}
 ls_data[3] = {name: 'Onesss', age: 16.5}
 ls_data[4] = {name: 'test', age: 22}
 
+fSyncData()
 function fSyncData() {
 	localStorage.setItem(ls_name, JSON.stringify(ls_data))
 	createDataTable(ls_data)
 }
-
-fSyncData()
 
 function createDataTable(items) {
 	const data_elements = items.map((item, id) => {
@@ -26,11 +25,11 @@ function createDataTable(items) {
 
 		const tdAct = document.createElement('td')
 		tdAct.innerHTML = `
-			<button id="edit_${id}">
+			<button id="edit_${id}" onclick="setItem(${id})">
 				Edit
 			</button>
-			<button id="delete_${id}">
-				Edit
+			<button id="delete_${id}" onclick="deleteData(${id})">
+				Delete
 			</button>			
 		`
 
@@ -44,7 +43,48 @@ function createDataTable(items) {
 	})
 
 	const tbody = document.getElementById('items')
-	// tbody.innerHTML = ""
-
+	tbody.innerHTML = ""
 	data_elements.forEach(data => tbody.appendChild(data))
+}
+
+function getItem () {
+	const id = document.getElementById('id').value
+	const name = document.getElementById('name').value
+	const age = document.getElementById('age').value
+
+	return {id, name, age}
+}
+
+function setItem(id) {
+	document.getElementById('id').value = id
+	document.getElementById('name').value = ls_data[id].name
+	document.getElementById('age').value = ls_data[id].age
+}
+
+function getLastId() {
+	const str_data = localStorage.getItem('final_assesment_ls')
+	const arr_data = JSON.parse(str_data)
+	const total_data = arr_data.length
+	return total_data
+}
+
+function fUpsert() {
+	const data = getItem()
+	const id = parseInt(data.id) || getLastId()
+
+	ls_data[id] = { name: data.name, age: data.age }
+
+	fSyncData()
+	clearForm()
+}
+
+function deleteData (id) {
+	delete ls_data[id]
+	fSyncData()
+}
+
+function clearForm() {
+	document.getElementById('id').value = ""
+	document.getElementById('name').value = ""
+	document.getElementById('age').value = ""
 }
